9bb386994d81549db93aa72619fbaa759a140d86
NekoSarada1101 <ryoyukikazu11@gmail.com>
2021-02-06 13:04:08 +0900

    feat: 値がNoneなら追加しない

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ def main(data, context):
 
     contributions = []  # type: list
     for tags in soup.find_all("rect"):
-        contributions.append(tags.get('data-count'))
+        if tags.get('data-count') is not None:
+            contributions.append(tags.get('data-count'))
     print(contributions)
 
     today = contributions[len(contributions) - 1]  # type: str
